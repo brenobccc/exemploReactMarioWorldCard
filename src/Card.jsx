4b5c5898376abf9b83848cba6447454967b6588a
brenobccc <brenobritobc@gmail.com>
2025-05-09 06:59:35 -0300

    projeto

---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Card.css';
+
+export default function Card(){
+    return (
+        <div id="Card">
+            <h1>Este é meu card</h1>
+        </div>
+    )
+}
